c46e784567e7ec6efb4d48099c0733b294bc3909
Alberto Bautista <albertbg@MacBook-Pro-de-Alberto.local>
2026-09-11 12:08:25 -0600

    added changes to jenkinsfile

--- src/App.js
+++ src/App.js
@@ -12,11 +12,14 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy
+          Learn Jenkins on Udemy 
         </a>
       </header>
       <p>
           Application version: 1
+      </p>
+      <p>
+          Albert Wood.          
       </p>
     </div>
   );
